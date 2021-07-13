4b258bd260005e3461d1290083ac3fd864319752
Dinesh Chukkala <dineshchukkala169@gmail.com>
2021-07-14 03:33:32 +0530

    loading and error pages

--- src/Components/CnxError.js
+++ src/Components/CnxError.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function CnxError() {
-    return <div>Error</div>
+    return (
+        <div className='error'>
+            <div className='error_text'>
+                There appears to be some network error :(
+            </div>
+            <div className='error_pre_link'>
+                Return Back to:
+                <Link to='/home'>
+                    <span className='error_link'> Home</span>
+                </Link>
+            </div>
+        </div>
+    )
 }
--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function Loading() {
-    return <div>Loading</div>
+    return (
+        <div className='loading'>
+            <div className='loading_main_circle'></div>
+        </div>
+    )
 }
